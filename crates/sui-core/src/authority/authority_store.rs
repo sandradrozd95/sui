0411eef1ac75e99b03fd7f22772db58af22c4e0c
Xun Li <lxfind@gmail.com>
2022-06-19 16:48:54 -0700

    [epoch] Revert effects that did not reach finality

--- crates/sui-core/src/authority/authority_store.rs
+++ crates/sui-core/src/authority/authority_store.rs
@@ -1,3 +1,4 @@
+use std::iter;
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 use super::*;
@@ -9,6 +10,7 @@ use rocksdb::Options;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use std::path::Path;
+use std::ptr::write;
 use sui_storage::{
     default_db_options,
     mutex_table::{LockGuard, MutexTable},
@@ -854,6 +856,40 @@ impl<const ALL_OBJ_VER: bool, S: Eq + Serialize + for<'de> Deserialize<'de>>
         };
 
         Ok(assigned_seq)
+    }
+
+    /// This function is called at the end of epoch for each transaction that's
+    /// executed locally on the validator but didn't make to the last checkpoint.
+    /// The effects of the execution is reverted here.
+    /// The following things are reverted:
+    /// 1. Certificate and effects are deleted.
+    /// 2. Latest parent_sync entries for each mutated object are deleted.
+    /// 3. All new object states are deleted.
+    pub fn revert_state_update(&self, tx_digest: &TransactionDigest) -> SuiResult {
+        let effects = self.get_effects(tx_digest)?;
+        let mut write_batch = self.certificates.batch();
+        write_batch = write_batch.delete_batch(&self.certificates, iter::once(tx_digest))?;
+        write_batch = write_batch.delete_batch(&self.effects, iter::once(tx_digest))?;
+
+        let all_new_refs = effects
+            .mutated
+            .iter()
+            .chain(effects.created.iter())
+            .chain(effects.unwrapped.iter())
+            .map(|(r, _)| r)
+            .chain(effects.deleted.iter())
+            .chain(effects.wrapped.iter());
+        write_batch = write_batch.delete_batch(&self.parent_sync, all_new_refs)?;
+
+        let all_new_object_keys = effects
+            .mutated
+            .iter()
+            .chain(effects.created.iter())
+            .chain(effects.unwrapped.iter())
+            .map(|((id, version, _), _)| ObjectKey(*id, *version));
+        write_batch = write_batch.delete_batch(&self.objects, all_new_object_keys)?;
+        write_batch.write()?;
+        Ok(())
     }
 
     /// Returns the last entry we have for this object in the parents_sync index used
